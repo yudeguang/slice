17a7a7b4fe233ea278395a0db790bb8ae37aae48
yudeguang <84558040@qq.com>
2020-03-24 21:41:16 +0800

    update DeleteByIndex

--- delete_by_index.go
+++ delete_by_index.go
@@ -1,141 +1,211 @@
-// DeleteByIndexright 2020 slice Author(https://github.com/yudeguang/slice). All Rights Reserved.
+// DeleteSubSliceright 2020 slice Author(https://github.com/yudeguang/slice). All Rights Reserved.
 //
 // This Source Code Form is subject to the terms of the MIT License.
-// If a DeleteByIndex of the MIT was not distributed with this file,
+// If a DeleteSubSlice of the MIT was not distributed with this file,
 // You can obtain one at https://github.com/yudeguang/slice.
 package slice
 
 /*
 // 第一种方法（保持剩余元素的次序）：
-s = append(copyS[:i], copyS[i+1:]...)
+s = append(s[:from], s[to:]...)
 
 // 第二种方法（保持剩余元素的次序）：
-s = s[:i + copy(s[i:], s[i+1:])]
-
-// 上面两种方法都需要复制len(s)-i-1个元素。
+s = s[:from + copy(s[from:], s[to:])]
 
 // 第三种方法（不保持剩余元素的次序）：
-s[i] = s[len(s)-1]
-s = s[:len(s)-1]
+if n := to-from; len(s)-to < n {
+	copy(s[from:to], s[to:])
+} else {
+	copy(s[from:to], s[len(s)-n:])
+}
+s = s[:len(s)-(to-from)]
+//如果切片的元素可能引用着其它值，则我们应该重置因为删除元素而多出来的元素槽位上的元素值，以避免暂时性的内存泄露：
+// "len(s)+to-from"是删除操作之前切片s的长度。
+temp := s[len(s):len(s)+to-from]
+for i := range temp {
+	temp[i] = t0 // t0是类型T的零值
+}
 
-//在此，只实现第一种方法
+//在此，只实现第一种方法，并且使用安全一些的方法，保持原有切片内容不变
+
 */
-//删除切片中第i个元素
-func DeleteByIndex(s []interface{}, i int) []interface{} {
+
+//删除从位置是从from（包括）到to（不包括）的相关元素,如果to不填写，则表示只删除from这个位置的一个元素
+func DeleteSubSlice(s []interface{}, from int, to ...int) []interface{} {
 	copyS := Copy(s)
-	return append(copyS[:i], copyS[i+1:]...)
+	if len(to) == 0 {
+		return append(copyS[:from], copyS[from+1:]...)
+	}
+	return append(copyS[:from], copyS[to[0]:]...)
 }
 
-//删除切片中第i个元素
-func DeleteByIndexString(s []string, i int) []string {
+//删除从位置是从from（包括）到to（不包括）的相关元素,如果to不填写，则表示只删除from这个位置的一个元素
+func DeleteSubSliceString(s []string, from int, to ...int) []string {
 	copyS := CopyString(s)
-	return append(copyS[:i], copyS[i+1:]...)
+	if len(to) == 0 {
+		return append(copyS[:from], copyS[from+1:]...)
+	}
+	return append(copyS[:from], copyS[to[0]:]...)
 }
 
-//删除切片中第i个元素
-func DeleteByIndexInt(s []int, i int) []int {
+//删除从位置是从from（包括）到to（不包括）的相关元素,如果to不填写，则表示只删除from这个位置的一个元素
+func DeleteSubSliceInt(s []int, from int, to ...int) []int {
 	copyS := CopyInt(s)
-	return append(copyS[:i], copyS[i+1:]...)
+	if len(to) == 0 {
+		return append(copyS[:from], copyS[from+1:]...)
+	}
+	return append(copyS[:from], copyS[to[0]:]...)
 }
 
-//删除切片中第i个元素
-func DeleteByIndexInt8(s []int8, i int) []int8 {
+//删除从位置是从from（包括）到to（不包括）的相关元素,如果to不填写，则表示只删除from这个位置的一个元素
+func DeleteSubSliceInt8(s []int8, from int, to ...int) []int8 {
 	copyS := CopyInt8(s)
-	return append(copyS[:i], copyS[i+1:]...)
+	if len(to) == 0 {
+		return append(copyS[:from], copyS[from+1:]...)
+	}
+	return append(copyS[:from], copyS[to[0]:]...)
 }
 
-//删除切片中第i个元素
-func DeleteByIndexInt16(s []int16, i int) []int16 {
+//删除从位置是从from（包括）到to（不包括）的相关元素,如果to不填写，则表示只删除from这个位置的一个元素
+func DeleteSubSliceInt16(s []int16, from int, to ...int) []int16 {
 	copyS := CopyInt16(s)
-	return append(copyS[:i], copyS[i+1:]...)
+	if len(to) == 0 {
+		return append(copyS[:from], copyS[from+1:]...)
+	}
+	return append(copyS[:from], copyS[to[0]:]...)
 }
 
-//删除切片中第i个元素
-func DeleteByIndexInt32(s []int32, i int) []int32 {
+//删除从位置是从from（包括）到to（不包括）的相关元素,如果to不填写，则表示只删除from这个位置的一个元素
+func DeleteSubSliceInt32(s []int32, from int, to ...int) []int32 {
 	copyS := CopyInt32(s)
-	return append(copyS[:i], copyS[i+1:]...)
+	if len(to) == 0 {
+		return append(copyS[:from], copyS[from+1:]...)
+	}
+	return append(copyS[:from], copyS[to[0]:]...)
 }
 
-//删除切片中第i个元素
-func DeleteByIndexInt64(s []int64, i int) []int64 {
+//删除从位置是从from（包括）到to（不包括）的相关元素,如果to不填写，则表示只删除from这个位置的一个元素
+func DeleteSubSliceInt64(s []int64, from int, to ...int) []int64 {
 	copyS := CopyInt64(s)
-	return append(copyS[:i], copyS[i+1:]...)
+	if len(to) == 0 {
+		return append(copyS[:from], copyS[from+1:]...)
+	}
+	return append(copyS[:from], copyS[to[0]:]...)
 }
 
-//删除切片中第i个元素
-func DeleteByIndexUint(s []uint, i int) []uint {
+//删除从位置是从from（包括）到to（不包括）的相关元素,如果to不填写，则表示只删除from这个位置的一个元素
+func DeleteSubSliceUint(s []uint, from int, to ...int) []uint {
 	copyS := CopyUint(s)
-	return append(copyS[:i], copyS[i+1:]...)
+	if len(to) == 0 {
+		return append(copyS[:from], copyS[from+1:]...)
+	}
+	return append(copyS[:from], copyS[to[0]:]...)
 }
 
-//删除切片中第i个元素
-func DeleteByIndexUint8(s []uint8, i int) []uint8 {
+//删除从位置是从from（包括）到to（不包括）的相关元素,如果to不填写，则表示只删除from这个位置的一个元素
+func DeleteSubSliceUint8(s []uint8, from int, to ...int) []uint8 {
 	copyS := CopyUint8(s)
-	return append(copyS[:i], copyS[i+1:]...)
+	if len(to) == 0 {
+		return append(copyS[:from], copyS[from+1:]...)
+	}
+	return append(copyS[:from], copyS[to[0]:]...)
 }
 
-//删除切片中第i个元素
-func DeleteByIndexUint16(s []uint16, i int) []uint16 {
+//删除从位置是从from（包括）到to（不包括）的相关元素,如果to不填写，则表示只删除from这个位置的一个元素
+func DeleteSubSliceUint16(s []uint16, from int, to ...int) []uint16 {
 	copyS := CopyUint16(s)
-	return append(copyS[:i], copyS[i+1:]...)
+	if len(to) == 0 {
+		return append(copyS[:from], copyS[from+1:]...)
+	}
+	return append(copyS[:from], copyS[to[0]:]...)
 }
 
-//删除切片中第i个元素
-func DeleteByIndexUint32(s []uint32, i int) []uint32 {
+//删除从位置是从from（包括）到to（不包括）的相关元素,如果to不填写，则表示只删除from这个位置的一个元素
+func DeleteSubSliceUint32(s []uint32, from int, to ...int) []uint32 {
 	copyS := CopyUint32(s)
-	return append(copyS[:i], copyS[i+1:]...)
+	if len(to) == 0 {
+		return append(copyS[:from], copyS[from+1:]...)
+	}
+	return append(copyS[:from], copyS[to[0]:]...)
 }
 
-//删除切片中第i个元素
-func DeleteByIndexUint64(s []uint64, i int) []uint64 {
+//删除从位置是从from（包括）到to（不包括）的相关元素,如果to不填写，则表示只删除from这个位置的一个元素
+func DeleteSubSliceUint64(s []uint64, from int, to ...int) []uint64 {
 	copyS := CopyUint64(s)
-	return append(copyS[:i], copyS[i+1:]...)
+	if len(to) == 0 {
+		return append(copyS[:from], copyS[from+1:]...)
+	}
+	return append(copyS[:from], copyS[to[0]:]...)
 }
 
-//删除切片中第i个元素
-func DeleteByIndexUintptr(s []uintptr, i int) []uintptr {
+//删除从位置是从from（包括）到to（不包括）的相关元素,如果to不填写，则表示只删除from这个位置的一个元素
+func DeleteSubSliceUintptr(s []uintptr, from int, to ...int) []uintptr {
 	copyS := CopyUintptr(s)
-	return append(copyS[:i], copyS[i+1:]...)
+	if len(to) == 0 {
+		return append(copyS[:from], copyS[from+1:]...)
+	}
+	return append(copyS[:from], copyS[to[0]:]...)
 }
 
-//删除切片中第i个元素
-func DeleteByIndexFloat32(s []float32, i int) []float32 {
+//删除从位置是从from（包括）到to（不包括）的相关元素,如果to不填写，则表示只删除from这个位置的一个元素
+func DeleteSubSliceFloat32(s []float32, from int, to ...int) []float32 {
 	copyS := CopyFloat32(s)
-	return append(copyS[:i], copyS[i+1:]...)
+	if len(to) == 0 {
+		return append(copyS[:from], copyS[from+1:]...)
+	}
+	return append(copyS[:from], copyS[to[0]:]...)
 }
 
-//删除切片中第i个元素
-func DeleteByIndexFloat64(s []float64, i int) []float64 {
+//删除从位置是从from（包括）到to（不包括）的相关元素,如果to不填写，则表示只删除from这个位置的一个元素
+func DeleteSubSliceFloat64(s []float64, from int, to ...int) []float64 {
 	copyS := CopyFloat64(s)
-	return append(copyS[:i], copyS[i+1:]...)
+	if len(to) == 0 {
+		return append(copyS[:from], copyS[from+1:]...)
+	}
+	return append(copyS[:from], copyS[to[0]:]...)
 }
 
-//删除切片中第i个元素
-func DeleteByIndexRune(s []rune, i int) []rune {
+//删除从位置是从from（包括）到to（不包括）的相关元素,如果to不填写，则表示只删除from这个位置的一个元素
+func DeleteSubSliceRune(s []rune, from int, to ...int) []rune {
 	copyS := CopyRune(s)
-	return append(copyS[:i], copyS[i+1:]...)
+	if len(to) == 0 {
+		return append(copyS[:from], copyS[from+1:]...)
+	}
+	return append(copyS[:from], copyS[to[0]:]...)
 }
 
-//删除切片中第i个元素
-func DeleteByIndexByte(s []byte, i int) []byte {
+//删除从位置是从from（包括）到to（不包括）的相关元素,如果to不填写，则表示只删除from这个位置的一个元素
+func DeleteSubSliceByte(s []byte, from int, to ...int) []byte {
 	copyS := CopyByte(s)
-	return append(copyS[:i], copyS[i+1:]...)
+	if len(to) == 0 {
+		return append(copyS[:from], copyS[from+1:]...)
+	}
+	return append(copyS[:from], copyS[to[0]:]...)
 }
 
-//删除切片中第i个元素
-func DeleteByIndexComplex64(s []complex64, i int) []complex64 {
+//删除从位置是从from（包括）到to（不包括）的相关元素,如果to不填写，则表示只删除from这个位置的一个元素
+func DeleteSubSliceComplex64(s []complex64, from int, to ...int) []complex64 {
 	copyS := CopyComplex64(s)
-	return append(copyS[:i], copyS[i+1:]...)
+	if len(to) == 0 {
+		return append(copyS[:from], copyS[from+1:]...)
+	}
+	return append(copyS[:from], copyS[to[0]:]...)
 }
 
-//删除切片中第i个元素
-func DeleteByIndexComplex128(s []complex128, i int) []complex128 {
+//删除从位置是从from（包括）到to（不包括）的相关元素,如果to不填写，则表示只删除from这个位置的一个元素
+func DeleteSubSliceComplex128(s []complex128, from int, to ...int) []complex128 {
 	copyS := CopyComplex128(s)
-	return append(copyS[:i], copyS[i+1:]...)
+	if len(to) == 0 {
+		return append(copyS[:from], copyS[from+1:]...)
+	}
+	return append(copyS[:from], copyS[to[0]:]...)
 }
 
-//删除切片中第i个元素
-func DeleteByIndexBool(s []bool, i int) []bool {
+//删除从位置是从from（包括）到to（不包括）的相关元素,如果to不填写，则表示只删除from这个位置的一个元素
+func DeleteSubSliceBool(s []bool, from int, to ...int) []bool {
 	copyS := CopyBool(s)
-	return append(copyS[:i], copyS[i+1:]...)
+	if len(to) == 0 {
+		return append(copyS[:from], copyS[from+1:]...)
+	}
+	return append(copyS[:from], copyS[to[0]:]...)
 }
