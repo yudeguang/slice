d110bc8ce90316dd243038ee2361cca1aeb2caba
yudeguang <84558040@qq.com>
2020-03-24 21:50:21 +0800

    update DeleteByIndex

--- delete_by_index.go
+++ delete_by_index.go
@@ -1,7 +1,7 @@
-// DeleteSubSliceright 2020 slice Author(https://github.com/yudeguang/slice). All Rights Reserved.
+// DeleteByIndexright 2020 slice Author(https://github.com/yudeguang/slice). All Rights Reserved.
 //
 // This Source Code Form is subject to the terms of the MIT License.
-// If a DeleteSubSlice of the MIT was not distributed with this file,
+// If a DeleteByIndex of the MIT was not distributed with this file,
 // You can obtain one at https://github.com/yudeguang/slice.
 package slice
 
@@ -31,7 +31,7 @@ for i := range temp {
 */
 
 //删除从位置是从from（包括）到to（不包括）的相关元素,如果to不填写，则表示只删除from这个位置的一个元素
-func DeleteSubSlice(s []interface{}, from int, to ...int) []interface{} {
+func DeleteByIndex(s []interface{}, from int, to ...int) []interface{} {
 	copyS := Copy(s)
 	if len(to) == 0 {
 		return append(copyS[:from], copyS[from+1:]...)
@@ -40,7 +40,7 @@ func DeleteSubSlice(s []interface{}, from int, to ...int) []interface{} {
 }
 
 //删除从位置是从from（包括）到to（不包括）的相关元素,如果to不填写，则表示只删除from这个位置的一个元素
-func DeleteSubSliceString(s []string, from int, to ...int) []string {
+func DeleteByIndexString(s []string, from int, to ...int) []string {
 	copyS := CopyString(s)
 	if len(to) == 0 {
 		return append(copyS[:from], copyS[from+1:]...)
@@ -49,7 +49,7 @@ func DeleteSubSliceString(s []string, from int, to ...int) []string {
 }
 
 //删除从位置是从from（包括）到to（不包括）的相关元素,如果to不填写，则表示只删除from这个位置的一个元素
-func DeleteSubSliceInt(s []int, from int, to ...int) []int {
+func DeleteByIndexInt(s []int, from int, to ...int) []int {
 	copyS := CopyInt(s)
 	if len(to) == 0 {
 		return append(copyS[:from], copyS[from+1:]...)
@@ -58,7 +58,7 @@ func DeleteSubSliceInt(s []int, from int, to ...int) []int {
 }
 
 //删除从位置是从from（包括）到to（不包括）的相关元素,如果to不填写，则表示只删除from这个位置的一个元素
-func DeleteSubSliceInt8(s []int8, from int, to ...int) []int8 {
+func DeleteByIndexInt8(s []int8, from int, to ...int) []int8 {
 	copyS := CopyInt8(s)
 	if len(to) == 0 {
 		return append(copyS[:from], copyS[from+1:]...)
@@ -67,7 +67,7 @@ func DeleteSubSliceInt8(s []int8, from int, to ...int) []int8 {
 }
 
 //删除从位置是从from（包括）到to（不包括）的相关元素,如果to不填写，则表示只删除from这个位置的一个元素
-func DeleteSubSliceInt16(s []int16, from int, to ...int) []int16 {
+func DeleteByIndexInt16(s []int16, from int, to ...int) []int16 {
 	copyS := CopyInt16(s)
 	if len(to) == 0 {
 		return append(copyS[:from], copyS[from+1:]...)
@@ -76,7 +76,7 @@ func DeleteSubSliceInt16(s []int16, from int, to ...int) []int16 {
 }
 
 //删除从位置是从from（包括）到to（不包括）的相关元素,如果to不填写，则表示只删除from这个位置的一个元素
-func DeleteSubSliceInt32(s []int32, from int, to ...int) []int32 {
+func DeleteByIndexInt32(s []int32, from int, to ...int) []int32 {
 	copyS := CopyInt32(s)
 	if len(to) == 0 {
 		return append(copyS[:from], copyS[from+1:]...)
@@ -85,7 +85,7 @@ func DeleteSubSliceInt32(s []int32, from int, to ...int) []int32 {
 }
 
 //删除从位置是从from（包括）到to（不包括）的相关元素,如果to不填写，则表示只删除from这个位置的一个元素
-func DeleteSubSliceInt64(s []int64, from int, to ...int) []int64 {
+func DeleteByIndexInt64(s []int64, from int, to ...int) []int64 {
 	copyS := CopyInt64(s)
 	if len(to) == 0 {
 		return append(copyS[:from], copyS[from+1:]...)
@@ -94,7 +94,7 @@ func DeleteSubSliceInt64(s []int64, from int, to ...int) []int64 {
 }
 
 //删除从位置是从from（包括）到to（不包括）的相关元素,如果to不填写，则表示只删除from这个位置的一个元素
-func DeleteSubSliceUint(s []uint, from int, to ...int) []uint {
+func DeleteByIndexUint(s []uint, from int, to ...int) []uint {
 	copyS := CopyUint(s)
 	if len(to) == 0 {
 		return append(copyS[:from], copyS[from+1:]...)
@@ -103,7 +103,7 @@ func DeleteSubSliceUint(s []uint, from int, to ...int) []uint {
 }
 
 //删除从位置是从from（包括）到to（不包括）的相关元素,如果to不填写，则表示只删除from这个位置的一个元素
-func DeleteSubSliceUint8(s []uint8, from int, to ...int) []uint8 {
+func DeleteByIndexUint8(s []uint8, from int, to ...int) []uint8 {
 	copyS := CopyUint8(s)
 	if len(to) == 0 {
 		return append(copyS[:from], copyS[from+1:]...)
@@ -112,7 +112,7 @@ func DeleteSubSliceUint8(s []uint8, from int, to ...int) []uint8 {
 }
 
 //删除从位置是从from（包括）到to（不包括）的相关元素,如果to不填写，则表示只删除from这个位置的一个元素
-func DeleteSubSliceUint16(s []uint16, from int, to ...int) []uint16 {
+func DeleteByIndexUint16(s []uint16, from int, to ...int) []uint16 {
 	copyS := CopyUint16(s)
 	if len(to) == 0 {
 		return append(copyS[:from], copyS[from+1:]...)
@@ -121,7 +121,7 @@ func DeleteSubSliceUint16(s []uint16, from int, to ...int) []uint16 {
 }
 
 //删除从位置是从from（包括）到to（不包括）的相关元素,如果to不填写，则表示只删除from这个位置的一个元素
-func DeleteSubSliceUint32(s []uint32, from int, to ...int) []uint32 {
+func DeleteByIndexUint32(s []uint32, from int, to ...int) []uint32 {
 	copyS := CopyUint32(s)
 	if len(to) == 0 {
 		return append(copyS[:from], copyS[from+1:]...)
@@ -130,7 +130,7 @@ func DeleteSubSliceUint32(s []uint32, from int, to ...int) []uint32 {
 }
 
 //删除从位置是从from（包括）到to（不包括）的相关元素,如果to不填写，则表示只删除from这个位置的一个元素
-func DeleteSubSliceUint64(s []uint64, from int, to ...int) []uint64 {
+func DeleteByIndexUint64(s []uint64, from int, to ...int) []uint64 {
 	copyS := CopyUint64(s)
 	if len(to) == 0 {
 		return append(copyS[:from], copyS[from+1:]...)
@@ -139,7 +139,7 @@ func DeleteSubSliceUint64(s []uint64, from int, to ...int) []uint64 {
 }
 
 //删除从位置是从from（包括）到to（不包括）的相关元素,如果to不填写，则表示只删除from这个位置的一个元素
-func DeleteSubSliceUintptr(s []uintptr, from int, to ...int) []uintptr {
+func DeleteByIndexUintptr(s []uintptr, from int, to ...int) []uintptr {
 	copyS := CopyUintptr(s)
 	if len(to) == 0 {
 		return append(copyS[:from], copyS[from+1:]...)
@@ -148,7 +148,7 @@ func DeleteSubSliceUintptr(s []uintptr, from int, to ...int) []uintptr {
 }
 
 //删除从位置是从from（包括）到to（不包括）的相关元素,如果to不填写，则表示只删除from这个位置的一个元素
-func DeleteSubSliceFloat32(s []float32, from int, to ...int) []float32 {
+func DeleteByIndexFloat32(s []float32, from int, to ...int) []float32 {
 	copyS := CopyFloat32(s)
 	if len(to) == 0 {
 		return append(copyS[:from], copyS[from+1:]...)
@@ -157,7 +157,7 @@ func DeleteSubSliceFloat32(s []float32, from int, to ...int) []float32 {
 }
 
 //删除从位置是从from（包括）到to（不包括）的相关元素,如果to不填写，则表示只删除from这个位置的一个元素
-func DeleteSubSliceFloat64(s []float64, from int, to ...int) []float64 {
+func DeleteByIndexFloat64(s []float64, from int, to ...int) []float64 {
 	copyS := CopyFloat64(s)
 	if len(to) == 0 {
 		return append(copyS[:from], copyS[from+1:]...)
@@ -166,7 +166,7 @@ func DeleteSubSliceFloat64(s []float64, from int, to ...int) []float64 {
 }
 
 //删除从位置是从from（包括）到to（不包括）的相关元素,如果to不填写，则表示只删除from这个位置的一个元素
-func DeleteSubSliceRune(s []rune, from int, to ...int) []rune {
+func DeleteByIndexRune(s []rune, from int, to ...int) []rune {
 	copyS := CopyRune(s)
 	if len(to) == 0 {
 		return append(copyS[:from], copyS[from+1:]...)
@@ -175,7 +175,7 @@ func DeleteSubSliceRune(s []rune, from int, to ...int) []rune {
 }
 
 //删除从位置是从from（包括）到to（不包括）的相关元素,如果to不填写，则表示只删除from这个位置的一个元素
-func DeleteSubSliceByte(s []byte, from int, to ...int) []byte {
+func DeleteByIndexByte(s []byte, from int, to ...int) []byte {
 	copyS := CopyByte(s)
 	if len(to) == 0 {
 		return append(copyS[:from], copyS[from+1:]...)
@@ -184,7 +184,7 @@ func DeleteSubSliceByte(s []byte, from int, to ...int) []byte {
 }
 
 //删除从位置是从from（包括）到to（不包括）的相关元素,如果to不填写，则表示只删除from这个位置的一个元素
-func DeleteSubSliceComplex64(s []complex64, from int, to ...int) []complex64 {
+func DeleteByIndexComplex64(s []complex64, from int, to ...int) []complex64 {
 	copyS := CopyComplex64(s)
 	if len(to) == 0 {
 		return append(copyS[:from], copyS[from+1:]...)
@@ -193,7 +193,7 @@ func DeleteSubSliceComplex64(s []complex64, from int, to ...int) []complex64 {
 }
 
 //删除从位置是从from（包括）到to（不包括）的相关元素,如果to不填写，则表示只删除from这个位置的一个元素
-func DeleteSubSliceComplex128(s []complex128, from int, to ...int) []complex128 {
+func DeleteByIndexComplex128(s []complex128, from int, to ...int) []complex128 {
 	copyS := CopyComplex128(s)
 	if len(to) == 0 {
 		return append(copyS[:from], copyS[from+1:]...)
@@ -202,7 +202,7 @@ func DeleteSubSliceComplex128(s []complex128, from int, to ...int) []complex128 
 }
 
 //删除从位置是从from（包括）到to（不包括）的相关元素,如果to不填写，则表示只删除from这个位置的一个元素
-func DeleteSubSliceBool(s []bool, from int, to ...int) []bool {
+func DeleteByIndexBool(s []bool, from int, to ...int) []bool {
 	copyS := CopyBool(s)
 	if len(to) == 0 {
 		return append(copyS[:from], copyS[from+1:]...)
